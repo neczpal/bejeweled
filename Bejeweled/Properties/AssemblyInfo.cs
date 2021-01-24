89d4581546324ee9db8a809245d6c329ff310962
Neczpal Abel <neczpal@gmail.com>
2021-01-24 11:38:15 +0100

    bugfix: rename caused the project to be not openable

--- Bejeweled/Properties/AssemblyInfo.cs
+++ Bejeweled/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bejewelled blitz")]
+[assembly: AssemblyTitle("Bejeweled")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bejewelled blitz")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Bejeweled")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
